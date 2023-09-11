b503bcd41c8c9a2b4bfe9fc9a30c389b009e9c6f
Felipe Cadar Chamone <cadar@dcc.ufmg.br>
2023-09-11 10:10:20 +0200

    update hub

--- hubconf.py
+++ hubconf.py
@@ -2,10 +2,10 @@ dependencies = ['torch', 'torchvision']
 from models.our_detector import Our
 import torch
 
-# resnet18 is the name of entrypoint
-def resnet18(pretrained=False, **kwargs):
+# Detector is the name of entrypoint
+def Detector(pretrained=False, **kwargs):
     """ # This docstring shows up in hub.help()
-    Resnet18 model
+    PRL 2023 Detector model
     pretrained (bool): kwargs, load pretrained weights into the model
     """
     # Call the model, load pretrained weights
@@ -14,6 +14,5 @@ def resnet18(pretrained=False, **kwargs):
     if pretrained:
         checkpoint = 'https://github.com/verlab/LearningToDetect_PRL_2023/raw/main/pretrained/our/final_model_1500.pth'
         model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint, progress=False))
-
 
     return model
